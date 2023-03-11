1bcc83fa16376a40aba77ca60b4096fff81e7cfd
Shihan Mohamed <shihanmails@gmail.com>
2023-03-11 10:54:59 +0530

    Rep Updates

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using AngularLogin.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private AppDbContext _authDbContext;
+
+        public CustomerController(AppDbContext appDbContext)
+        {
+            _authDbContext = appDbContext;
+        }
+        [HttpPost("RegisterLot")]
+        public async Task<IActionResult> RegisterLocation([FromBody] Lot LotObj)
+        {
+            if (LotObj == null)
+                return BadRequest();
+
+            if (await CheckLotIdExistAsync(LotObj.LotId))
+                return BadRequest(new { Message = "Lot ID Already Exist" });
+
+            await _authDbContext.Lots.AddAsync(LotObj);
+            await _authDbContext.SaveChangesAsync();
+            return Ok(new { Message = "Lot Registered Successfully" });
+        }
+        private Task<bool> CheckLotIdExistAsync(int lotId)
+            => _authDbContext.Lots.AnyAsync(x => x.LotId == lotId);
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<Location>> GetAllLots()
+        {
+            return Ok(await _authDbContext.Lots.ToListAsync());
+        }
+
+        [HttpPut("updatelots")]
+        public async Task<IActionResult> UpdateLot([FromBody] Lot LotObj)
+        {
+            if (LotObj == null)
+                return BadRequest();
+
+            var lot = await _authDbContext.Lots.AsNoTracking().FirstOrDefaultAsync(x => x.LotId == LotObj.LotId);
+            if (lot == null)
+            {
+                return BadRequest(new
+                {
+                    StatusCode = 400,
+                    Message = "Lot Not Found"
+                });
+            }
+            else
+            {
+                _authDbContext.Lots.Update(LotObj);
+                await _authDbContext.SaveChangesAsync();
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "Lot Updated Successfully"
+                });
+            }
+        }
+    }
+}
---
+++ Controllers/RepController.cs
@@ -0,0 +1,73 @@
+﻿using AngularLogin.Context;
+using AngularLogin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularLogin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepController : Controller
+    {
+        private AppDbContext _authDbContext;
+
+        public RepController(AppDbContext appDbContext)
+        {
+            _authDbContext = appDbContext;
+        }
+        [HttpPost("RegisterRep")]
+        public async Task<IActionResult> RegisterRep([FromBody] Rep RepObj)
+        {
+            if (RepObj == null)
+                return BadRequest();
+
+            if (await CheckRepIdExistAsync(RepObj.RepId))
+                return BadRequest(new { Message = "Rep ID Already Exist" });
+
+            await _authDbContext.Reps.AddAsync(RepObj);
+            await _authDbContext.SaveChangesAsync();
+            return Ok(new { Message = "Rep Registered Successfully" });
+        }
+        private Task<bool> CheckRepIdExistAsync(int RepId)
+            => _authDbContext.Reps.AnyAsync(x => x.RepId == RepId);
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<Location>> GetAllReps()
+        {
+            return Ok(await _authDbContext.Reps.ToListAsync());
+        }
+
+        [HttpPut("updateReps")]
+        public async Task<IActionResult> UpdateRep([FromBody] Rep RepObj)
+        {
+            if (RepObj == null)
+                return BadRequest();
+
+            var rep = await _authDbContext.Reps.AsNoTracking().FirstOrDefaultAsync(x => x.RepId == RepObj.RepId);
+            if (rep == null)
+            {
+                return BadRequest(new
+                {
+                    StatusCode = 400,
+                    Message = "Rep Not Found"
+                });
+            }
+            else
+            {
+                _authDbContext.Reps.Update(RepObj);
+                await _authDbContext.SaveChangesAsync();
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "Rep Updated Successfully"
+                });
+            }
+        }
+    }
+}
